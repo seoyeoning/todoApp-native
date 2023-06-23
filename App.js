6c592e6415d5eee493487821365f80527b0127b5
seoyeoning <minalunpark@naver.com>
2023-06-23 22:14:55 +0900

    first

---
+++ App.js
@@ -0,0 +1,22 @@
+/// App.js 화면에 보는이는 것들 구현
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
